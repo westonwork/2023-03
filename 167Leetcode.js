d478d7ae65ff2c9cf4ff3492f7f239c40d47e020
Emily Weston <emmylombardo@aol.com>
2023-03-13 12:45:55 -0400

    167LC Two Pointers v2

--- 167Leetcode.js
+++ 167Leetcode.js
@@ -3,18 +3,55 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// Using two pointers approach since the array is sorted in non-dessccending order
 var twoSum = function (numbers, target) {
-    newArr = []
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = 1; j < numbers.length; j++) {
-            console.log(numbers[i], numbers[j]);
-            if ((numbers[i] + numbers[j]) === target) {
-                i += 1;
-                j += 1;
-                newArr.push(i, j);
-                return newArr;
-            }
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = (numbers[left] + numbers[right]);
+        if (sum < target) {
+            left++;
+        } else if (sum > target) {
+            right--;
+        } else {
+            return [left + 1, right + 1]
         }
     }
-    // return newArr;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var twoSum2 = function (numbers, target) {
+    // newArr = []
+    // for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = 1; j < numbers.length; j++) {
+    //         console.log(numbers[i], numbers[j]);
+    //         if ((numbers[i] + numbers[j]) === target) {
+    //             i += 1;
+    //             j += 1;
+    //             newArr.push(i, j);
+    //             return newArr;
+    //         }
+    //     }
+    // }
+    // return newArr;
+// };
