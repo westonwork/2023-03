a65fcb455bd32f414664d22c5bc9844e99eacddc
Emily Weston <emmylombardo@aol.com>
2023-03-15 16:25:16 -0400

    15LC Submitted Solution

--- 15Leetcode.js
+++ 15Leetcode.js
@@ -8,20 +8,56 @@
  * @return {number[][]}
  */
  var threeSum = function(nums) {
-    let left = 0;
-    let right = nums.length-1;
-    let result = []
+    /**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const result = [];
+    nums.sort((a,b) => a-b)
 
-    while (left < right) {
-        if (nums.length === 3) {
-            let sum = nums[0] + nums[1] + nums[2];
-            if (sum != 0){
-                return [];
+    for(let i = 0; i < nums.length; i++){
+        const a = nums[i];
+        if (a > 0) break;
+        if (i > 0 && a === nums[i -1]) continue;
+        let left = i+1;
+        let right = nums.length -1;
+
+        while (left < right) {
+            let sum = a + nums[left] + nums[right];
+            if (sum === 0) {
+                result.push([a, nums[left], nums[right]]);
+                left++;
+                right--;
+                while (nums[left] === nums[left-1] && left < right) {
+                    left++;
+                }
+            } else if (sum < 0) {
+                left++
             } else {
-                return [nums];
+                right--
             }
-        } else {
-            return result;
         }
     }
+    return result;
+    };
+    
+
+    // let left = 0;
+    // let right = nums.length-1;
+    // let result = []
+
+    // while (left < right) {
+    //     if (nums.length === 3) {
+    //         let sum = nums[0] + nums[1] + nums[2];
+    //         if (sum != 0){
+    //             return [];
+    //         } else {
+    //             return [nums];
+    //         }
+    //     } else {
+    //         return result;
+    //     }
+    // }
+
 };
