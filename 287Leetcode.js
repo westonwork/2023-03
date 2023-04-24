6797d1a894c30a9110b46f8870719785c423c7e5
Emily Weston <emmylombardo@aol.com>
2023-04-24 11:08:33 -0400

    287 LC

--- 287Leetcode.js
+++ 287Leetcode.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
- var findDuplicate = function(nums) {
+    var findDuplicate = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] === nums[j]) {
